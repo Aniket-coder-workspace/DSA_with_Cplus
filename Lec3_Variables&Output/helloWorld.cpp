379509254b165d1c56843617cd6ff95a2f2ce60b
Aniket <aneecoder9789@gmail.com>
2024-07-09 22:49:03 +0530

    file updated

--- Lec3_Variables&Output/helloWorld.cpp
+++ Lec3_Variables&Output/helloWorld.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 int main(){
+    
+    // cout is used for printing in c++
     cout<<"Hello World"<<endl;
     cout<<"Hello Anee's Mac"<<endl;
+
+    //Variable decleration in c++
+    int apple = 4; //Here (int - datatype) (apple - variable) (4 - value)
+    cout<<apple;
     return 0;
 }
