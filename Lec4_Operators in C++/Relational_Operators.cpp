352bc50d0cf5e026e13b484d97e97edeb37a2107
Aniket <aneecoder9789@gmail.com>
2024-07-10 21:09:00 +0530

    File added

---
+++ Lec4_Operators in C++/Relational_Operators.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num1 = 84;
+    int num2 = 15;
+
+    cout<<(num1==num2)<<endl; // Output - 0 (False)
+    cout<<(num1!=num2)<<endl; // Output - 1 (True)
+    cout<<(num1>=num2)<<endl; // Output - 1 (True)
+    cout<<(num1<=num2)<<endl; // Output - 0 (False)
+    
+    return 0;
+}
