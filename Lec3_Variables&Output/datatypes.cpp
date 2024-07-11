fe84ab921263cd51bcd808c2f11a29123d15b98d
Aniket <aneecoder9789@gmail.com>
2024-07-11 14:23:28 +0530

    file updated

--- Lec3_Variables&Output/datatypes.cpp
+++ Lec3_Variables&Output/datatypes.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 int main(){
     cout<<"Hello Anee"<<endl;
+
+    int num = 5;
+    char a = 'A';
+    bool b = true;
+    float v = 7.89;
+    double c = 7.898989890;
+    string name = "Anee's Mac";
+
+    cout<<num<<endl; // Output 5
+    cout<<a<<endl; // Output A
+    cout<<b<<endl; // Output true
+    cout<<v<<endl; // Output 7.89
+    cout<<c<<endl; // Output 7.89899
+    cout<<name<<endl; // Output Anee's Mac
     return 0;
 }
