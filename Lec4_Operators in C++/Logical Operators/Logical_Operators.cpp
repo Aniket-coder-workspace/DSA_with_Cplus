b53be62aad118c1668c2360e98db9812d6ae55ce
Aniket <aneecoder9789@gmail.com>
2024-07-14 11:36:24 +0530

    file updated

--- Lec4_Operators in C++/Logical Operators/Logical_Operators.cpp
+++ Lec4_Operators in C++/Logical Operators/Logical_Operators.cpp
@@ -3,5 +3,11 @@ using namespace std;
 
 int main(){
     cout<<"This is logical operators"<<endl;
+    bool exp1 = true;
+    bool exp2 = false;
+    cout<<(exp1&&exp2)<<endl; // False
+    cout<<(exp1&&exp2)<<endl; // False
+    cout<<(!exp1)<<endl; // False
+    cout<<(!exp2)<<endl; // True
     return 0;
 }
