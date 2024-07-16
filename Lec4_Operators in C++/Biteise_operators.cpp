34b126477dd44c36dabe822fb5c04055780e4768
Aniket <aneecoder9789@gmail.com>
2024-07-16 12:00:25 +0530

    file updated

---
+++ Lec4_Operators in C++/Biteise_operators.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 5;
+
+    cout<<(a<<1)<<endl; // after multiplied by 2 output 10
+    cout<<(a>>1)<<endl; // after dividing by 2 output 2
+
+    return 0;
+}
