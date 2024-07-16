77688b603b17ea8a7b010ec7f98341f8c087fe48
Aniket <aneecoder9789@gmail.com>
2024-07-16 21:45:20 +0530

    file updated

---
+++ Lec4_Operators in C++/Post_Pre_Operators.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 5;
+    cout<<a++<<endl; // Output is 5
+
+    int b = 5;
+    cout<<++b<<endl; // Output is 6
+    return 0;
+}
