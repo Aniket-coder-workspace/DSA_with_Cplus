6d28eec7c60ca175d4f9da3acba0e65913466fdb
Aniket <aneecoder9789@gmail.com>
2024-07-18 12:35:09 +0530

    a new file add

---
+++ Practice_of_C++/Practice1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"Hello Anee"<<endl;
+    //Datatypes in C++
+    int num = 56;
+    char a = 'A';
+    string name = "Aniket";
+    bool value = true; // this can be (ture or false)
+    
+    return 0;
+}
