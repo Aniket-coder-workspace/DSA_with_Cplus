3affde77d17eb6aa2d336f64f32823137a3aafa9
Aniket <aneecoder9789@gmail.com>
2024-07-28 10:11:55 +0530

    file updated

---
+++ Practice_of_C++/rough.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 45;
+    char b = 'C';
+    float num = 567.90;
+    double num1 = 67.909090;
+    string name = "Aniket";
+    bool value = true;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<num<<endl;
+    cout<<num1<<endl;
+    cout<<name<<endl;
+    cout<<value<<endl;
+    return 0;
+}
