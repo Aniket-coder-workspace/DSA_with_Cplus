ca86cededaae6abd724d4ba19a7cdd6e6f68cb39
Aniket <aneecoder9789@gmail.com>
2024-08-01 22:25:26 +0530

    file updated

--- Practice_of_C++/rough.cpp
+++ Practice_of_C++/rough.cpp
@@ -2,17 +2,37 @@
 using namespace std;
 
 int main(){
-    int a = 45;
-    char b = 'C';
-    float num = 567.90;
-    double num1 = 67.909090;
-    string name = "Aniket";
-    bool value = true;
-    cout<<a<<endl;
-    cout<<b<<endl;
-    cout<<num<<endl;
-    cout<<num1<<endl;
-    cout<<name<<endl;
-    cout<<value<<endl;
+    // int a = 45;
+    // char b = 'C';
+    // float num = 567.90;
+    // double num1 = 67.909090;
+    // string name = "Aniket";
+    // bool value = true;
+    // cout<<a<<endl;
+    // cout<<b<<endl;
+    // cout<<num<<endl;
+    // cout<<num1<<endl;
+    // cout<<name<<endl;
+    // cout<<value<<endl;
+
+    // int b = 67;
+    // bool value1 = true;
+    // float c = 67.18;
+    // string cd = "Aniket";
+    // char vh = 't';
+    // double n = 78.1234;
+
+    int x;
+    int b;
+    cin>>x;
+    cin>>b;
+    if (x == 5){
+        cout<<"Digit is called 5";
+    }else{
+        cout<<"Digit is not called 5";
+    }
+
+
+
     return 0;
 }
